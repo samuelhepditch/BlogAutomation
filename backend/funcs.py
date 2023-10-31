7499df597db23bf115dd41816d3be7846ea237fe
AndreiSec <andreisecara01@gmail.com>
2023-10-31 17:57:42 -0400

    Update

---
+++ backend/funcs.py
@@ -0,0 +1,13 @@
+from wp import WordPressApi
+from gpt import GPTApi
+
+def create_blog_post(title, keywords):
+
+    wpAPI = WordPressApi() 
+    availableCategories = wpAPI.get_categories()
+
+    gptAPI = GPTApi(test=False, categories=availableCategories)
+
+    blogPost = gptAPI.create_blog_post(title, keywords)
+
+    wpAPI.create_post(blogPost)
