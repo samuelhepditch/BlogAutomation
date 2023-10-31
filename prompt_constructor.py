9c980823274402c29fb47313115762a34ad83580
Samuel Hepditch <hepditchsam@gmail.com>
2023-10-30 23:38:48 -0400

    Updating keywords

--- prompt_constructor.py
+++ prompt_constructor.py
@@ -7,5 +7,5 @@ class GPTKeywordPrompts(Enum):
 
 class GPTBlogPrompts(Enum):
     ROLE = "I want you to act as an internet health blogger"
-    BLOG = "You are a well-loved author of a popular blog that writes about women’s health and wellness. Please write an interesting click worthy article about {0}. Include these keywords: {1}. Keep the title simple yet click worthy."
+    BLOG = "You are a well-loved author of a popular blog that writes about women’s health and wellness. Please write an interesting click worthy article about {0}. Include these keywords: {1}. Keep the title very short, simple and click worthy."
     CATEGORIES = "I will give you a list of categories for a health blog. Based on this post's title: '{0}', what category would you put this post into? Here are the categories: {1}. Just give the category name."
