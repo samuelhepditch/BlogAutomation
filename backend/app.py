b4e00f66f3e6e11c3d1d9beb4490f68267e81678
AndreiSec <andreisecara01@gmail.com>
2023-10-31 17:58:07 -0400

    App py update

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,16 @@
 from flask import Flask, render_template, request, jsonify
-from main import create_blog_post
+from flask_cors import CORS, cross_origin
+from wp import WordPressApi
+from gpt import GPTApi
+import time
+from funcs import create_blog_post
 import os
 
+
+
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 def get_used_keywords():
     with open('used_keywords.txt', 'r') as f:
@@ -17,27 +25,34 @@ def index():
     return render_template('index.html')
 
 @app.route('/write_blog', methods=['POST'])
+@cross_origin()
 def write_blog():
-    topic = request.form.get('topic')
-    keywords = request.form.get('keywords').split(',')
-    keywords.append(topic)
+    try:
+        body = request.get_json()
+        topic = body['topic']
+        keywords = body['keywords'].split(',')
+        keywords.append(topic)
 
-    used_keywords = get_used_keywords()
+        used_keywords = get_used_keywords()
 
-    for keyword in keywords:
-        keyword = keyword.strip()
-        
-        if keyword in used_keywords:
-            return jsonify({"status": "error", "message": f"Keyword '{keyword}' has been used before!"})
+        for keyword in keywords:
+            keyword = keyword.strip()
+            
+            if keyword in used_keywords:
+                return jsonify({"status": "error", "message": f"Keyword '{keyword}' has been used before!"})
 
-        # Add keyword to the file
-        add_keyword_to_file(keyword)
+            # Add keyword to the file
+            add_keyword_to_file(keyword)
 
-    # Call your main code to create a blog post
-    # create_blog_post(topic, keywords)
+        # Call your main code to create a blog post
+        create_blog_post(topic, keywords)
 
-    return jsonify({"status": "success", "message": "Blog post written successfully!"})
 
+        return jsonify({"status": "success", "message": "Blog post written successfully!"})
+    
+    except Exception as error:
+        return jsonify({'error': error})
+    
 if __name__ == "__main__":
     # Check if the text file exists, if not, create it
     if not os.path.exists('used_keywords.txt'):
